c70b1dda717a2b87f526bd42f746bc1c23e43ba8
Dawn E. Walker <Dawn@dawns-air.home>
2017-01-09 18:25:29 -0500

    Initial Commit

---
+++ GeoFencesCoupon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeoFencesCoupon
+//
+//  Created by Dawn E. Walker on 1/9/17.
+//  Copyright © 2017 Dawn E. Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeoFencesCoupon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeoFencesCoupon
+//
+//  Created by Dawn E. Walker on 1/9/17.
+//  Copyright © 2017 Dawn E. Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
